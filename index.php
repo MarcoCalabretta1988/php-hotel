84413017122f7d987b709cce76298331bc9a07e4
MarcoCalabretta1988 <calabretta.mar@gmail.com>
2023-02-07 15:44:40 +0100

    Add parking filter

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <?php
-
+//Array con i dati
 $hotels = [
 
     [
@@ -39,6 +39,11 @@ $hotels = [
     ],
 
 ];
+//Recupero scelta con checkbox
+$whit_parking = $_GET['whitparking'] ?? '';
+$vote = $_GET['vote'] ?? '';
+
+var_dump($whit_parking, $vote);
 
 ?>
 
@@ -53,30 +58,52 @@ $hotels = [
 
     <!-- IMPORT BOOTSTRAP -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
-
+    <style>
+        body {
+            background-image: url(./img/hotelbg.jpg);
+            background-size: cover;
+        }
+    </style>
 </head>
 
 <body>
-    <div class="container mt-5">
+    <div class="container mt-5 bg-white text-dark p-3 rounded-4">
         <h1>Boolean Hotels</h1>
         <hr>
-        <table class="table">
+        <h3>Filtra ricerca per:</h3>
+        <form action="" method="GET">
+            <div class="form-check">
+                <input class="form-check-input" type="checkbox" value="true" id="whit_parking" name="whitparking">
+                <label class="form-check-label" for="whit_parking">Parcheggio </label>
+            </div>
+
+            <div class="my-3">
+
+                <button class="btn btn-primary">Cerca</button>
+                <a href="http://localhost:8080/php-hotel" class="btn btn-danger">Annulla</a>
+            </div>
+
+        </form>
+        <hr>
+        <table class="table text-center">
             <thead>
                 <tr>
                     <?php foreach ($hotels[0] as $key => $hotel) : ?>
-                        <th scope="col"><?= $key ?></th>
+                        <th scope="col"><?= strtoupper($key) ?></th>
                     <? endforeach; ?>
                 </tr>
             </thead>
             <tbody>
                 <?php foreach ($hotels as $key => $hotel) : ?>
-                    <tr>
-                        <th scope="row"><?= $hotel['name'] ?></th>
-                        <td><?= $hotel['description'] ?></td>
-                        <td><?= $hotel['parking'] ?></td>
-                        <td><?= $hotel['vote'] ?></td>
-                        <td><?= $hotel['distance_to_center'] ?></td>
-                    </tr>
+                    <?php if ($whit_parking == $hotel['parking'] || !$whit_parking) : ?>
+                        <tr>
+                            <th scope="row"><?= $hotel['name'] ?></th>
+                            <td><?= $hotel['description'] ?></td>
+                            <td><?= $hotel['parking'] ?></td>
+                            <td><?= $hotel['vote'] ?></td>
+                            <td><?= $hotel['distance_to_center'] ?></td>
+                        </tr>
+                    <?php endif; ?>
             </tbody>
         <? endforeach; ?>
         </table>
